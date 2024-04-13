441ae368f455e7523c29df077b2f0e117a7da14c
gergo-m <119097427+gergo-m@users.noreply.github.com>
2024-04-13 07:19:36 +0200

    registering and display in profile done

--- profile.php
+++ profile.php
@@ -23,39 +23,39 @@
             <h1>Profile</h1>
         </div>
         <div class="textinput">
-            <input type="text" id="fname" name="fname" value="<?php echo $_SESSION['user_name']; ?>" autocomplete="off">
+            <input type="text" id="first_name" name="first_name" value="<?php echo $_SESSION['first_name']; ?>" autocomplete="off">
             <label for="fname">First name</label>
         </div>
         <div class="textinput">
-            <input type="text" id="lname" name="lname" value="<?php echo $_SESSION['name']; ?>" autocomplete="off">
+            <input type="text" id="last_name" name="last_name" value="<?php echo $_SESSION['last_name']; ?>" autocomplete="off">
             <label for="lname">Last name</label>
         </div>
         <div class="textinput">
-            <input type="date" id="bdate" name="bdate" value="2000-01-01" autocomplete="off">
+            <input type="date" id="birth_date" name="birth_date" value="<?php echo $_SESSION['birth_date']; ?>" autocomplete="off">
             <label for="bdate">Birth date</label>
         </div>
         <div class="textinput">
-            <input type="email" id="email" name="email" value="johndoe97@gmail.com" autocomplete="off">
+            <input type="email" id="email" name="email" value="<?php echo $_SESSION['email']; ?>" autocomplete="off">
             <label for="email">Email address</label>
         </div>
         <div class="textinput">
-            <input type="password" id="password" name="password" value="password1234" autocomplete="off">
+            <input type="password" id="password" name="password" value="" autocomplete="off">
             <label for="password">Password</label>
         </div>
         <div>
-            <input type="checkbox" id="business_owner" name="account_type" value="business_owner" checked="checked">
+            <input type="checkbox" id="business_owner" name="account_type" value="<?php echo $_SESSION['account_type']; ?>" checked="checked">
             <label for="business_owner">I'm a business owner</label>
         </div>
         <div class="textinput">
             <input type="tel" id="phone" name="phone" value="+36301234567" autocomplete="off">
             <label for="phone">Phone number</label>
         </div>
-        <?php if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) { ?>
+        <?php if (isset($_SESSION['id']) && isset($_SESSION['email'])) { ?>
             <div>
                 <a href="logout.php">Logout</a>
             </div>
         <?php } else {
-            header("Location: test.php");
+            header("Location: register.php?error=Account type is " . $_POST["account_type"]);
             exit();
         } ?>
     </div>
--- register_db.php
+++ register_db.php
@@ -6,13 +6,16 @@ if (isset($_POST["first_name"])
     && isset($_POST["email"])
     && isset($_POST["password"])
     && isset($_POST["birth_date"])
-    && isset($_POST["phone_number"])
-    && isset($_POST["account_type"])) {
+    && isset($_POST["phone_number"])) {
     function validate($data) {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
+    }
+
+    if (!isset($_POST["account_type"])) {
+        $_POST["account_type"] = 0;
     }
 
     $first_name = validate($_POST["first_name"]);
@@ -41,49 +44,54 @@ if (isset($_POST["first_name"])
     } else if (empty($phone_number)) {
         header("Location: register.php?error=Phone number is required");
         exit();
-    } else if (empty($account_type)) {
-        header("Location: register.php?error=Account type is required");
-        exit();
     } else {
+        $conn = $GLOBALS["db_connection"];
         $sqlCheckIfExists = "SELECT * FROM users
                 WHERE email='$email'";
-        $resultIfExists = mysqli_query($GLOBALS["db_connection"], $sqlCheckIfExists);
+        $resultIfExists = mysqli_query($conn, $sqlCheckIfExists);
         echo mysqli_num_rows($resultIfExists);
         if (mysqli_num_rows($resultIfExists) === 1) {
             header("Location: register.php?error=User already exists with this email");
             exit();
         }
 
-        $sql = "INSERT INTO `users`(`id`, `first_name`, `last_name`, `email`, `password`, `birth_date`, `phone_number`, `business_owner`) VALUES ('','$first_name','$last_name','$email','$password','$birth_date','$phone_number','$account_type')";
-        $result = mysqli_query($GLOBALS["db_connection"], $sql);
-        echo $result;
-        if ($result === 1) {
-            $row = mysqli_fetch_assoc($result);
-            if ($row['first_name'] === $first_name
-                && $row['last_name'] === $last_name
-                && $row['email'] === $email
-                && $row['password'] === $password
-                && $row['birth_date'] === $birth_date
-                && $row['phone_number'] === $phone_number
-                && $row['account_type'] === $account_type) {
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-                echo "Registered and logged in!";
-                $_SESSION['id'] = $row['id'];
-                $_SESSION['first_name'] = $row['first_name'];
-                $_SESSION['last_name'] = $row['last_name'];
-                $_SESSION['email'] = $row['email'];
-                $_SESSION['birth_date'] = $row['birth_date'];
-                $_SESSION['account_type'] = $row['account_type'];
-                header("Location: profile.php");
+        $sql = "INSERT INTO `users`(`id`, `first_name`, `last_name`, `email`, `password`, `birth_date`, `phone_number`, `business_owner`) VALUES ('','$first_name','$last_name','$email','$password','$birth_date','$phone_number','$account_type')";
+        if ($conn->query($sql) === TRUE) {
+            $result = mysqli_query($conn, $sqlCheckIfExists);
+            if ($result instanceof mysqli_result) {
+                $row = mysqli_fetch_assoc($result);
+                if ($row['first_name'] === $first_name
+                    && $row['last_name'] === $last_name
+                    && $row['email'] === $email
+                    && $row['password'] === $password
+                    && $row['birth_date'] === $birth_date
+                    && $row['phone_number'] === $phone_number
+                    && $row['business_owner'] === $account_type) {
+
+                    echo "Registered and logged in!";
+                    $_SESSION['id'] = $row['id'];
+                    $_SESSION['first_name'] = $row['first_name'];
+                    $_SESSION['last_name'] = $row['last_name'];
+                    $_SESSION['email'] = $row['email'];
+                    $_SESSION['birth_date'] = $row['birth_date'];
+                    $_SESSION['account_type'] = $row['business_owner'];
+                    header("Location: profile.php");
+                    exit();
+                }
+            } else {
+                header("Location: register.php?error=Something is wrong " . $result);
                 exit();
             }
         } else {
-            header("Location: register.php?error=Something is wrong");
-            exit();
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
     }
 } else {
-    header("Location: profile.php");
+    header("Location: profile.php?error=Account type is " . $_POST["account_type"]);
     exit();
 }
 ?>
