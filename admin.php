11260576ba7da964ad7d74276d465ef389aae591
GyurisAron <164160822+GyurisAron@users.noreply.github.com>
2024-04-19 15:53:08 +0200

    admin 0

---
+++ admin.php
@@ -0,0 +1,39 @@
+
+
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+<meta charset="UTF-8">
+<title>Admin</title>
+<link rel="icon" type="image/x-icon" href="./assets/profile-pictures/icon-admin.png">
+</head>
+<body>
+
+
+
+
+    
+    <form method="post">
+        <label >
+            Accept
+            <input type="button">
+        </label>
+        <label>
+            Rejects
+            <input type="button">
+        </label>
+
+    </form>
+    <form method="post">
+        <label>
+            Delet businesses
+        </label>
+    </form>
+
+    <form method="post">
+        <label>
+            Ban User
+        </label>
+    </form>
+</html>
