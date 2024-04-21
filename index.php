c8407f764efff099253dd8dbdad9d3505f93a35f
GyurisAron <164160822+GyurisAron@users.noreply.github.com>
2024-04-21 21:25:40 +0200

    validating

--- index.php
+++ index.php
@@ -6,16 +6,15 @@
     <link rel="icon" type="image/x-icon" href="./assets/images-icons/favicon.webp">
     <link rel="stylesheet" href="style.css">
 </head>
+
+
 <body>
-<?php include_once "header.php";
-include_once "topnav.php";
-include_once "db_connection.php"; ?>
-<body>
+<?php include_once "header.php";include_once "topnav.php";include_once "db_connection.php"; ?>
 <div class="welcome-container">
     <h1>Welcome!</h1>
     <p>You should check about and businesses.</p>
 </div>
 
-</body>
+
 </body>
 </html>
